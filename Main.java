0dc05fcf61c18698f1fff821013cdab8f343a77a
Benjaminatedu <91627659+Benjaminatedu@users.noreply.github.com>
2023-05-20 23:19:17 -0600

    Added main code

---
+++ Main.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+
+
+public class Main {
+public static void main(String[] args) {  
+    List<Hashtable<String, String>> monsterList = convertCSV("data.txt");
+    searchForMonster(monsterList);
+   
+}
+
+public static List<Hashtable<String, String>> convertCSV(String args){
+    List<Hashtable<String, String>> lines = new ArrayList<Hashtable<String,String>>();
+    try (BufferedReader br = new BufferedReader(new FileReader("data.csv"))) {
+    
+        // CSV file delimiter
+        String DELIMITER = ",";
+    
+        // read the file line by line
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] columns = line.split(DELIMITER);
+            Hashtable<String, String> monsterDictionary = new Hashtable<>(); {
+                monsterDictionary.put("Name", columns[0]);
+                monsterDictionary.put("URL", columns[1]);
+                monsterDictionary.put("CR", columns[2]);
+                monsterDictionary.put("Type", columns[3]);
+                monsterDictionary.put("Size", columns[4]);
+                monsterDictionary.put("AC", columns[5]);
+                monsterDictionary.put("HP", columns[6]);
+                monsterDictionary.put("Speed", columns[7]);
+                monsterDictionary.put("Align", columns[8]);
+                monsterDictionary.put("Legendary?", columns[9]);
+                monsterDictionary.put("Source", columns[10]);
+            };
+            lines.add(monsterDictionary);
+        }
+        return(lines);
+        
+    
+    } catch (IOException ex) {
+        ex.printStackTrace();
+    }
+
+    return(lines);
+
+}
+
+public static void searchForMonster(List<Hashtable<String, String>> monsterList){
+
+    Hashtable<String, String> categories = new Hashtable<String, String>();
+    categories.put("1", "Name");
+    categories.put("2", "CR");
+    categories.put("3", "Type");
+    categories.put("4", "Size");
+    categories.put("5", "AC");
+    categories.put("6", "HP");
+    categories.put("7", "Align");
+
+    try(Scanner myObj = new Scanner(System.in)){  // Create a Scanner object
+        System.out.printf("Categories%n1. Name%n2. Challenge Rating%n3. Type%n4. Size%n5. Armor Class%n6. Health%n7. Alignment%n8. Exit%nWhat category would you like to search by?: %n");
+        String categoryChoice = myObj.nextLine();  // Read user input
+        
+        if(categoryChoice.equals("8")){
+            System.out.println("Thank You for Your Time!");
+        }
+        else if(!categoryChoice.matches("1|2|3|4|5|6|7")){
+            System.out.println("Please Try Again!");
+            searchForMonster(monsterList);
+        }
+        else{
+            System.out.printf("Search in " + categories.get(categoryChoice) + ": ");
+            String searchCriteria = myObj.nextLine();
+
+            for(Hashtable<String,String> monster: monsterList){
+                if((monster.get(categories.get(categoryChoice))).equals(searchCriteria)){
+                    System.out.println(monster);
+                }
+            }
+            searchForMonster(monsterList);
+        }
+        
+    }
+}
+}
